540773d4523ef268a7b22a640593af7fd0c61143
mayank <mayank.suraitya@swaica.com>
2026-02-11 15:41:04 +0530

    2

--- src/firebase.js
+++ src/firebase.js
@@ -8,15 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB6QdY8k96rMbZg9Ao0tCR_I50fzv2YzT4",
-    authDomain: "portfolio-714d7.firebaseapp.com",
-    projectId: "portfolio-714d7",
-    storageBucket: "portfolio-714d7.firebasestorage.app",
-    messagingSenderId: "977266235720",
-    appId: "1:977266235720:web:224f0d2b466ec502a3bb7d",
-    measurementId: "G-2754M22WPW"
+    apiKey: "AIzaSyCq21i_Wq92o1smSfTGbnWrBeZn6anWbSA",
+    authDomain: "portfolio-49124.firebaseapp.com",
+    projectId: "portfolio-49124",
+    storageBucket: "portfolio-49124.firebasestorage.app",
+    messagingSenderId: "384371128",
+    appId: "1:384371128:web:dffd0cbf43386d0e2e674d",
+    measurementId: "G-1BP2XXYE34"
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
